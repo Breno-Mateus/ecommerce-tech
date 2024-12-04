ee5587ebd56827f5844b1ef22563a9b52b2c4bf8
Breno-Mateus <brenomateus36@gmail.com>
2024-12-04 11:52:27 -0300

    feat: modifying the section-products for mobile

--- src/components/section-products/index.tsx
+++ src/components/section-products/index.tsx
@@ -7,7 +7,7 @@ const SectionProducts = ({filterProducts, category} : sectionProductsProps) => {
     const filteredProducts = products.filter(product => product.category === category);
 
     return (
-        <section className="mx-28 my-12">
+        <section className="mx-12 md:mx-28 my-12">
             <div className="flex gap-10 flex-wrap justify-center">
                 {filterProducts === true ? (
                     <>
