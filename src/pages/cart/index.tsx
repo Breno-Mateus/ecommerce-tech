a2d47dd756c0787938f1e4f9ee839cd77c7b6a97
Breno-Mateus <brenomateus36@gmail.com>
2024-12-05 10:17:51 -0300

    fix: change in the cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -8,105 +8,85 @@ import { useLogin } from "../../store/login-store";
 
 const Cart = () => {
 
-    const { productsCart, removeProduct, incrementQuantity, decrementQuantity, totalProduct, totalCart }  = useCart();
+    const { productsCart, removeProduct, incrementQuantity, decrementQuantity, totalProduct, totalCart, restartCart }  = useCart();
 
     const { currentUser } = useLogin();
+
+    const handleDisabled = (quantity : number) => {
+        return quantity === 1;
+    }
 
     return (
         <div>
             <Header />
             <MenuNav />
             {productsCart.length > 0 ? (
-                <section className="flex flex-col gap-8 mx-28 my-12">
-                    <div className="bg-colorGray flex items-center justify-between gap-8 p-2 rounded-md shadow-lg">
-                        <div className="w-[40%] text-center">
-                            <p>Produto</p>
+                <section className="flex flex-col gap-8 mx-28 my-12 min-h-screen justify-center">
+                    <div className="bg-colorGray flex flex-col gap-8 rounded-md shadow-md p-4">
+                        <div className="flex justify-between">
+                            <h1 className="font-semibold w-[50%]">Seu carrinho ({productsCart.length})</h1>
+                            <h1 className="font-semibold flex justify-center w-[25%]">Quantidade</h1>
+                            <h1 className="font-semibold flex justify-end w-[25%]">Preço</h1>
                         </div>
-                        <p>Quantidade</p>
-                        <p className="mr-2">Preço</p>
-                    </div>
-                    {productsCart.map((listaprodutos) => (
-                        <div key={listaprodutos.id} className="bg-colorGray flex items-center justify-between gap-8 p-8 rounded-md shadow-lg">
-                            <img src={listaprodutos.image} className="h-24"/>
-                            <p>{listaprodutos.name}</p>
-                            <div className="flex gap-2 text-colorSecondary">
-                                <div className="flex gap-2 p-2 bg-colorPrimary rounded-md">
-                                    <button onClick={() => decrementQuantity(listaprodutos.id)}>-</button>
-                                    <p>{listaprodutos.quantity}</p>
-                                    <button onClick={() => incrementQuantity(listaprodutos.id)} >+</button>    
-                                </div>
 
-                                <div className="bg-colorPrimary p-2 rounded-md flex items-center justify-center text-colorSecondary">
-                                    <button onClick={() => removeProduct(listaprodutos.id)}><FaTrash /></button>
+                        {productsCart.map((product) => (
+                        <div key={product.id} className={`flex items-center justify-between pb-4 ${productsCart.length > 1 ? "border-b-[1px] border-colorTextGray" : "border-none"}`}>
+                            <div className="flex gap-4 w-[50%]">
+                                <img src={product.image} className="w-24"/>
+                                
+                                <div className="flex flex-col gap-2">
+                                    <p className="font-semibold text-sm">{product.name}</p>
+                                    <p className="text-sm">Unid. R$ {product.price}</p>
+                                    <button className="font-semibold text-sm text-red-600 flex gap-2 items-center" onClick={() => removeProduct(product.id)}>Remover <FaTrash /></button>
                                 </div>
                             </div>
-
-                            <div >
-                                <p>R$ {totalProduct(listaprodutos.id)}</p>
+                            <div className="flex items-center justify-center gap-2 w-[25%]">
+                                <button onClick={() => decrementQuantity(product.id)} disabled={handleDisabled(product.quantity)} className={`${handleDisabled(product.quantity) === true ? "hover:cursor-not-allowed" : "hover:cursor-pointer"}`}>-</button>
+                                <div className="bg-colorSecondary rounded-md p-2 border-[1px] border-colorTextGray">
+                                    <p>{product.quantity}</p>
+                                </div>
+                                <button onClick={() => incrementQuantity(product.id)}>+</button>
+                            </div>
+                            <div className="w-[25%] flex justify-end">
+                                <p>R$ {totalProduct(product.id)}</p>
                             </div>
                         </div>
-                    ))}
-
-                    <div className="bg-colorGray flex flex-col gap-4 rounded-md shadow-lg p-8">
-                        <h3 className="font-bold">Resumo do pedido:</h3>
+                        ))}
                         
-                        <div className="flex flex-col border-b border-colorPrimary">
-                            <div className="flex justify-between">
-                                {productsCart.length === 1 ? (
-                                    <p>{productsCart.length} produto</p>
-                                ) : (<p>{productsCart.length} produtos</p>)}
-                                <p>R$ {totalCart()}</p>
+                        <div className="flex items-center justify-between">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex flex-col gap-2">
+                                    <h2 className="font-medium text-xl">Total</h2>
+                                    <h1 className="font-bold text-2xl">R$ {totalCart()}</h1>
+                                </div>
+                                <button className="flex gap-2 items-center text-red-600" onClick={() => restartCart()}>Esvaziar carrinho <FaTrash /></button>
                             </div>
-                            <div className="flex justify-between">
-                                <p className="text-green-700">frete</p>
-                                <p className="text-green-700">grátis</p>
-                            </div>
+                            <Link to={`${currentUser ? "/payment" : "/login"}`} className="bg-colorPrimary rounded-md text-colorSecondary p-4 h-[50%]">Continuar</Link>
                         </div>
-
-                        <div className="flex justify-between border-b border-colorPrimary">
-                            <p className="font-bold">Total</p>
-                            <p className="font-bold">R$ {totalCart()}</p>
-                        </div>
-
-                        <Link to={`${currentUser ? "/payment" : "/login"}`} className="bg-colorPrimary rounded-md text-colorSecondary p-2 hover:bg-opacity-90 w-[20%] flex items-center justify-center gap-6">Continuar</Link>
                     </div>
                 </section>
             ) : (
-                <section className="flex flex-col gap-8 mx-28 my-12">
-                    <div className="bg-colorGray flex items-center justify-between gap-8 p-2 rounded-md shadow-lg">
-                        <div className="w-[40%] text-center">
-                            <p>Produto</p>
+                <section className="flex flex-col gap-8 mx-28 my-12 min-h-screen justify-center">
+                    <div className="bg-colorGray flex flex-col gap-8 rounded-md shadow-md p-4">
+                        <div className="flex justify-between">
+                            <h1 className="font-semibold w-[50%]">Seu carrinho ({productsCart.length})</h1>
+                            <h1 className="font-semibold flex justify-center w-[25%]">Quantidade</h1>
+                            <h1 className="font-semibold flex justify-end w-[25%]">Preço</h1>
                         </div>
-                        <p>Quantidade</p>
-                        <p className="mr-2">Preço</p>
-                    </div>
 
-                    <div className="bg-colorGray flex items-center justify-center gap-8 p-8 rounded-md shadow-lg">
-                        <p>Ainda não há itens no carrinho!</p>
-                    </div>
-
-                    <div className="bg-colorGray flex flex-col gap-4 rounded-md shadow-lg p-8">
-                        <h3 className="font-bold">Resumo do pedido:</h3>
+                        <div className="flex items-center justify-center p-12">
+                            <h1>Ainda não há itens no carrinho!</h1>
+                        </div>
                         
-                        <div className="flex flex-col border-b border-colorPrimary">
-                            <div className="flex justify-between">
-                                {productsCart.length === 1 ? (
-                                    <p>{productsCart.length} produto</p>
-                                ) : (<p>{productsCart.length} produtos</p>)}
-                                <p>R$ {totalCart()}</p>
+                        <div className="flex items-center justify-between">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex flex-col gap-2">
+                                    <h2 className="font-medium text-xl">Total</h2>
+                                    <h1 className="font-bold text-2xl">R$ {totalCart()}</h1>
+                                </div>
                             </div>
-                            <div className="flex justify-between">
-                                <p className="text-green-700">frete</p>
-                                <p className="text-green-700">grátis</p>
-                            </div>
+                            <button className="bg-colorPrimary rounded-md text-colorSecondary p-4 h-[50%] bg-opacity-50 hover:cursor-not-allowed" disabled={true}>Continuar</button>
                         </div>
-
-                        <div className="flex justify-between border-b border-colorPrimary">
-                            <p className="font-bold">Total</p>
-                            <p className="font-bold">R$ {totalCart()}</p>
-                        </div>
-
-                        <button className="bg-colorPrimary rounded-md text-colorSecondary p-2 w-[20%] flex items-center justify-center gap-6 bg-opacity-50 hover:cursor-not-allowed">Continuar</button>
                     </div>
                 </section>
                 )
